6b4c9e39bc588671907ac5d8d7e05a8ae7d8beab
viveksingh07Cr <viveksingh424247@gmail.com>
2026-09-19 20:44:04 +0530

    Connect frontend to deployed backend

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
-    host: "0.0.0.0",
     port: 5173,
-    allowedHosts: ["192.168.1.35"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });
